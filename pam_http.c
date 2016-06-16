3339a4a8e17f04e4d49d8a77be5879c47481f415
Alexandr Lopatin <alexandr.lopatin@convergent-usa.com>
2016-06-16 17:39:46 +0400

    reformat code

--- pam_http.c
+++ pam_http.c
@@ -1,11 +1,11 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
 #include <string.h>
 #include <syslog.h>
 
-#include <libconfig.h>
 #include <curl/curl.h>
+#include <libconfig.h>
 #include <security/pam_appl.h>
 #include <security/pam_modules.h>
 
@@ -18,55 +18,52 @@
 #define DEFAULT_CURL_TIMEOUT 10
 
 static const char* HTTP_METHOD_POST = "POST";
-static const char* HTTP_METHOD_GET  = "GET";
+static const char* HTTP_METHOD_GET = "GET";
 
 static char password_prompt[] = "Password:";
 
-struct config_auth
-{
-    const char *c_auth_url;
-    const char *c_method;
-    const char *c_username_field;
-    const char *c_password_field;
+struct config_auth {
+    const char* c_auth_url;
+    const char* c_method;
+    const char* c_username_field;
+    const char* c_password_field;
     long long timeout;
 };
 
-static void pam_http_syslog(int priority, const char *format, ...)
+static void pam_http_syslog(int priority, const char* format, ...)
 {
     va_list args;
     va_start(args, format);
 
-    openlog("pam_http", LOG_CONS|LOG_PID, LOG_AUTH);
+    openlog("pam_http", LOG_CONS | LOG_PID, LOG_AUTH);
     vsyslog(priority, format, args);
     closelog();
 }
 
-static char* config_get_string(config_t* cfg, const char *name, const char *def_value)
+static char* config_get_string(config_t* cfg, const char* name, const char* def_value)
 {
-    const char *value;
+    const char* value;
     if (!config_lookup_string(cfg, name, &value))
         value = def_value;
 
     return strndup(value, BUFSIZE);
 }
 
-static int read_config_auth(char config_file[BUFSIZE], struct config_auth *s_auth)
+static int read_config_auth(char config_file[BUFSIZE], struct config_auth* s_auth)
 {
     config_t cfg;
-    const char *buf;
+    const char* buf;
 
     config_init(&cfg);
 
-    if (!config_read_file(&cfg, config_file))
-    {
-       pam_http_syslog(LOG_ALERT, "%s:%d - %s\n", config_error_file(&cfg),
-               config_error_line(&cfg), config_error_text(&cfg));
-       config_destroy(&cfg);
-       return 0;
+    if (!config_read_file(&cfg, config_file)) {
+        pam_http_syslog(LOG_ALERT, "%s:%d - %s\n", config_error_file(&cfg),
+            config_error_line(&cfg), config_error_text(&cfg));
+        config_destroy(&cfg);
+        return 0;
     }
 
-    if (!config_lookup_string(&cfg, "auth_url", &buf))
-    {
+    if (!config_lookup_string(&cfg, "auth_url", &buf)) {
         pam_http_syslog(LOG_ALERT, "No 'auth_url' setting in configuration file.");
         config_destroy(&cfg);
         return 0;
@@ -74,9 +71,7 @@ static int read_config_auth(char config_file[BUFSIZE], struct config_auth *s_aut
     s_auth->c_auth_url = strndup(buf, BUFSIZE);
 
     s_auth->c_method = config_get_string(&cfg, "auth_method", DEFAULT_METHOD);
-    if (strncmp(s_auth->c_method, HTTP_METHOD_GET, 3) != 0 &&
-        strncmp(s_auth->c_method, HTTP_METHOD_POST, 4) != 0)
-    {
+    if (strncmp(s_auth->c_method, HTTP_METHOD_GET, 3) != 0 && strncmp(s_auth->c_method, HTTP_METHOD_POST, 4) != 0) {
         pam_http_syslog(LOG_ALERT, "Wrong 'auth_method' setting in configuration file (%s).", s_auth->c_method);
         config_destroy(&cfg);
         return 0;
@@ -92,21 +87,19 @@ static int read_config_auth(char config_file[BUFSIZE], struct config_auth *s_aut
     return 1;
 }
 
-static int pam_http_get_user(pam_handle_t *pamh, const char **user)
+static int pam_http_get_user(pam_handle_t* pamh, const char** user)
 {
     int retval;
 
     retval = pam_get_user(pamh, user, NULL);
-    if (retval != PAM_SUCCESS)
-    {
-        pam_http_syslog(LOG_ALERT, "pam_get_user returns an error: %s", pam_strerror(pamh,retval));
+    if (retval != PAM_SUCCESS) {
+        pam_http_syslog(LOG_ALERT, "pam_get_user returns an error: %s", pam_strerror(pamh, retval));
         return retval;
     }
 
-    if (*user == NULL || **user == '\0')
-    {
+    if (*user == NULL || **user == '\0') {
         pam_http_syslog(LOG_ALERT, "undefined user");
-        retval = pam_set_item(pamh, PAM_USER, (const void *) DEFAULT_USER);
+        retval = pam_set_item(pamh, PAM_USER, (const void*)DEFAULT_USER);
         if (retval != PAM_SUCCESS)
             return PAM_USER_UNKNOWN;
     }
@@ -114,16 +107,16 @@ static int pam_http_get_user(pam_handle_t *pamh, const char **user)
     return retval;
 }
 
-static int pam_http_get_password(pam_handle_t *pamh, char **password)
+static int pam_http_get_password(pam_handle_t* pamh, char** password)
 {
     int retval, retry;
 
-    struct pam_conv *conv;
+    struct pam_conv* conv;
     struct pam_message msg;
-    const struct pam_message *msgp;
-    struct pam_response *resp;
+    const struct pam_message* msgp;
+    struct pam_response* resp;
 
-    retval = pam_get_item(pamh, PAM_CONV, (const void **)&conv);
+    retval = pam_get_item(pamh, PAM_CONV, (const void**)&conv);
     if (retval != PAM_SUCCESS)
         return PAM_SYSTEM_ERR;
 
@@ -158,9 +151,9 @@ static int pam_http_request(struct config_auth* s_auth, const char* user, const 
 {
     int retval;
 
-    CURL     *curl;
+    CURL* curl;
     CURLcode res;
-    long     http_code;
+    long http_code;
 
     char auth_url[BUFSIZE];
     char post_fields[BUFSIZE];
@@ -170,23 +163,19 @@ static int pam_http_request(struct config_auth* s_auth, const char* user, const 
     curl_global_init(CURL_GLOBAL_DEFAULT);
 
     curl = curl_easy_init();
-    if (curl)
-    {
+    if (curl) {
         esc_user = curl_easy_escape(curl, user, 0);
         esc_password = curl_easy_escape(curl, password, 0);
 
-        if (!strncmp(s_auth->c_method, HTTP_METHOD_GET, 3))
-        {
+        if (!strncmp(s_auth->c_method, HTTP_METHOD_GET, 3)) {
             has_parm = strstr(s_auth->c_auth_url, "?");
             sep = (has_parm == NULL) ? "?" : "&";
             snprintf(auth_url, BUFSIZE, "%s%s%s=%s&%s=%s", s_auth->c_auth_url, sep,
-                    s_auth->c_username_field, esc_user, s_auth->c_password_field, esc_password);
-        }
-        else
-        {
+                s_auth->c_username_field, esc_user, s_auth->c_password_field, esc_password);
+        } else {
             strncpy(auth_url, s_auth->c_auth_url, BUFSIZE);
             snprintf(post_fields, BUFSIZE, "%s=%s&%s=%s", s_auth->c_username_field,
-                    esc_user, s_auth->c_password_field, esc_password);
+                esc_user, s_auth->c_password_field, esc_password);
             curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_fields);
         }
 
@@ -197,14 +186,11 @@ static int pam_http_request(struct config_auth* s_auth, const char* user, const 
         curl_easy_setopt(curl, CURLOPT_TIMEOUT, s_auth->timeout);
 
         res = curl_easy_perform(curl);
-        if (res != CURLE_OK)
-        {
+        if (res != CURLE_OK) {
             pam_http_syslog(LOG_ALERT, "could not perform http request: %s %s:  %s",
-                    s_auth->c_method, auth_url, curl_easy_strerror(res));
+                s_auth->c_method, auth_url, curl_easy_strerror(res));
             retval = PAM_SERVICE_ERR;
-        }
-        else
-        {
+        } else {
             curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
             retval = http_code == 200 ? PAM_SUCCESS : PAM_AUTH_ERR;
         }
@@ -212,9 +198,7 @@ static int pam_http_request(struct config_auth* s_auth, const char* user, const 
         curl_free(esc_user);
         curl_free(esc_password);
         curl_easy_cleanup(curl);
-    }
-    else
-    {
+    } else {
         pam_http_syslog(LOG_ALERT, "could not init curl");
         retval = PAM_SYSTEM_ERR;
     }
@@ -224,27 +208,24 @@ static int pam_http_request(struct config_auth* s_auth, const char* user, const 
     return retval;
 }
 
-static int pam_http_authenticate(pam_handle_t *pamh, int argc, const char **argv)
+static int pam_http_authenticate(pam_handle_t* pamh, int argc, const char** argv)
 {
     int retval, i;
 
     char config_file[BUFSIZE];
     struct config_auth s_auth;
 
-    const char *user=NULL;
-    char *password;
+    const char* user = NULL;
+    char* password;
 
-    for (i = 0; i < argc; i++)
-    {
-        if (!strncmp(argv[i], "conf=", 5))
-        {
+    for (i = 0; i < argc; i++) {
+        if (!strncmp(argv[i], "conf=", 5)) {
             if (argv[i] + 5)
                 strncpy(config_file, argv[i] + 5, BUFSIZE - 2);
         }
     }
 
-    if (strlen(config_file) <= 1)
-    {
+    if (strlen(config_file) <= 1) {
         pam_http_syslog(LOG_ALERT, "conf directive is not set");
         return PAM_SYSTEM_ERR;
     }
@@ -265,38 +246,37 @@ static int pam_http_authenticate(pam_handle_t *pamh, int argc, const char **argv
     return retval;
 }
 
-int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
+int pam_sm_authenticate(pam_handle_t* pamh, int flags, int argc, const char** argv)
 {
     return pam_http_authenticate(pamh, argc, argv);
 }
 
-int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv)
+int pam_sm_setcred(pam_handle_t* pamh, int flags, int argc, const char** argv)
 {
     return PAM_SUCCESS;
 }
 
-int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
+int pam_sm_acct_mgmt(pam_handle_t* pamh, int flags, int argc, const char** argv)
 {
     return PAM_SUCCESS;
 }
 
-int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv)
+int pam_sm_chauthtok(pam_handle_t* pamh, int flags, int argc, const char** argv)
 {
     return PAM_SUCCESS;
 }
 
-int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc,
-            const char **argv)
+int pam_sm_open_session(pam_handle_t* pamh, int flags, int argc,
+    const char** argv)
 {
     return PAM_SUCCESS;
 }
 
-int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc,
-             const char **argv)
+int pam_sm_close_session(pam_handle_t* pamh, int flags, int argc,
+    const char** argv)
 {
     return PAM_SUCCESS;
 }
-
 
 #ifdef PAM_STATIC
 
